648072b33108e3ed12525a34624c6382838f94cf
momo-0826 <coco.bass.0826@gmail.com>
2024-04-20 10:58:30 +0900

    first comit

---
+++ src/main/kotlin/com/githubActionsImporterExample/GithubActionsImporterExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.githubActionsImporterExample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionsImporterExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GithubActionsImporterExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/githubActionsImporterExample/GithubActionsImporterExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.githubActionsImporterExample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionsImporterExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
